c7d06f84cbdef16e6e94754bb77da35eab452884
alonp13 <alonp13@gmail.com>
2020-01-09 19:57:27 +0200

    Final version

--- main.c
+++ main.c
@@ -3,42 +3,6 @@
 #include <string.h>
 #include "trie.h"
 
-//Read word from standart input. (taken from hw3)
-int getWord(char *w)
-{
-    char ch;
-    int num_count = 0;
-    while (scanf("%c", &ch) == 1)
-    {
-        if (num_count == WORD_SIZE)
-        {
-            w = (char *)realloc(w, WORD_SIZE * 2);
-        }
-
-        if (ch >= 'A' && ch <= 'Z') //ToLowerCase
-        {
-            ch += 32;
-            w[num_count] = ch;
-        }
-        else if ((ch == '\n') || (ch == '\t') || (ch == ' ') || (ch == 0)) //End of word
-        {
-            w[num_count] = ch;
-            break;
-        }
-        else if (ch >= 'a' && ch <= 'z') // Insert Low letter
-        {
-            w[num_count] = ch;
-        }
-        else //Ignore otherwise
-        {
-            continue;
-        }
-
-        num_count++; //count the letter added
-    }
-    return num_count;
-}
-
 int main(int argc, char *argv[])
 {
     //Check for invalid parameter. Need to be 'r' or nothing.
--- trie.h
+++ trie.h
@@ -2,32 +2,36 @@
 #define WORD_SIZE 10
 #define TO_LOWER 32
 
-typedef enum {FALSE=0, TRUE=1} boolean;
+typedef enum
+{
+    FALSE = 0,
+    TRUE = 1
+} boolean;
 
 typedef struct Node
 {
-    char letter;                            //represents the letter
-    long unsigned int count;                //num word ended by this node
-    struct Node * children[NUM_LETTERS];    //Node array, represents the potential children of this node
-    boolean isLeaf;                         //indicate whether this node is a end of word
+    char letter;                        //represents the letter
+    long unsigned int count;            //num word ended by this node
+    struct Node *children[NUM_LETTERS]; //Node array, represents the potential children of this node
+    boolean isLeaf;                     //indicate whether this node is a end of word
 } Node;
 
 typedef struct Trie
 {
-    Node * root;    //holds the trie root
+    Node *root; //holds the trie root
 } Trie;
 
-Trie* newTrie();
+Trie *newTrie();
 
-Node* newNode(char letter);
+Node *newNode(char letter);
 
-void insert(Trie* trie, char word[], int word_size);
+void insert(Trie *trie, char word[], int word_size);
 
-void printTrie(Trie * trie,int size);
-void display(Node * root, char str[], int level);
+void printTrie(Trie *trie, int size);
+void printTrieR(Trie *trie, int size);
 
-void printTrieR(Trie * trie,int size);
-void displayR(Node * root, char str[], int level);
+void display(Node *root, char str[], int level);
+void displayR(Node *root, char str[], int level);
 
-void freeTrie(Trie * trie);
-void freeNode(Node * node);
+void freeTrie(Trie *trie);
+void freeNode(Node *node);
